e51156f73475f330513289b7aad77d113828f429
Sriyan Nakka <sriyannakka@gmail.com>
2024-10-08 17:33:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,12 +7,38 @@ function coinFlipFunction() {
     console.log(randNum);
     switch(randNum){
         case 1:
-            document.querySelector("#headCoin").style.display = "block";
-            document.querySelector("#tailCoin").style.display = "none";
+            document.querySelector("#tailCoin").style.display = "none";            
+            document.querySelector("#result").textContent = "It is";
+            setTimeout(() => {
+            document.querySelector("#result").textContent = "It is.";
+            setTimeout(() => {
+                document.querySelector("#result").textContent = "It is..";
+                setTimeout(() => {
+                    document.querySelector("#result").textContent = "It is...";
+                    setTimeout(() => {
+                        document.querySelector("#result").textContent = "";
+                        document.querySelector("#headCoin").style.display = "block";
+                    }, 1510);    
+                }, 1500);    
+            }, 1000);    
+            }, 500);
             break;
         case 2:
-            document.querySelector("#headCoin").style.display = "none";
-            document.querySelector("#tailCoin").style.display = "block";
+            document.querySelector("#headCoin").style.display = "none";            
+            document.querySelector("#result").textContent = "It is";
+            setTimeout(() => {
+            document.querySelector("#result").textContent = "It is.";
+            setTimeout(() => {
+                document.querySelector("#result").textContent = "It is..";
+                setTimeout(() => {
+                    document.querySelector("#result").textContent = "It is...";
+                    setTimeout(() => {
+                        document.querySelector("#result").textContent = "";
+                        document.querySelector("#tailCoin").style.display = "block";
+                    }, 1510);    
+                }, 1500);    
+            }, 1000);    
+            }, 500);
             break;
     }
 }
